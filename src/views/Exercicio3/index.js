3f8523209a3e13540110b9a92535f23d06f0d212
Washington <washingtonluiz81@yahoo.com.br>
2021-01-18 12:41:48 -0300

    Finalizado a home já responsivo

--- src/views/Exercicio3/index.js
+++ src/views/Exercicio3/index.js
@@ -4,9 +4,54 @@ import React from "react";
 import Card from "../../components/Card";
 
 function Exercicio3() {
+  // const data = {
+  //   items: [{ value: 1 }, { value: 1 }],
+  // };
+
+  // function changeData(values) {
+  //   let localData = { ...values };
+
+  //   localData.items.map((i) => {
+  //     i.value = 2;
+  //   });
+
+  //   return localData;
+  // }
+
+  // const newData = changeData(data);
+
+  // console.log("data", data);
+  // console.log("newData", newData);
+
+  const data = {
+    items: [{ value: 1 }, { value: 1 }],
+  };
+
+  function changeData(values) {
+    let localData = JSON.parse(JSON.stringify({ ...values }));
+
+    localData.items.map((i) => {
+      i.value = 2;
+    });
+
+    return localData;
+  }
+
+  const newData = changeData(data);
+
+  console.log(data, newData);
   return (
     <Card title="Exercício 3">
-      <div>Exercício 3</div>
+      <div className="card-box-content-exercise">
+        <h6 className="font-weight-bold">
+          Qual o erro do código abaixo e o qual seria uma possível correção ?
+        </h6>
+
+        <div className="card-box-content-exercise--answer">
+          <h6>Resposta:</h6>
+          <p>respostasssssss</p>
+        </div>
+      </div>
     </Card>
   );
 }
--- src/views/index.js
+++ src/views/index.js
@@ -1,10 +1,66 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
 //CSS
 import "./index.css";
 
+//Images
+import ImageProfile from "../images/image-profile.jpg";
+import BgProfile from "../images/bg-profile.jpg";
+
 function Index() {
-  return <div className="container">Home Page</div>;
+  return (
+    <div className="container d-flex justify-content-center">
+      <div className="col-md-8 mt-5">
+        <div className="card profile-card">
+          <div className="card-img-block">
+            <img className="img-fluid" src={BgProfile} alt="Card image cap" />
+          </div>
+          <div className="card-body pt-5">
+            <img src={ImageProfile} alt="profile-image" className="profile" />
+            <h5 className="card-title ">Washington Luiz</h5>
+            <p className="card-text">
+              Apaixonado pela área da tecnologia, com foco no desenvolvimento
+              front-end nas tecnologias em crescimento exponencial como React,
+              NodeJS e React Native, sempre em busca de novos aprendizados e
+              conhecimentos.
+            </p>
+            <div className="icon-block">
+              <a
+                href="https://www.facebook.com/washington.ton81"
+                title="Facebook"
+                alt="Facebook"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a
+                href="https://www.instagram.com/washingtonluiz81/"
+                title="Instagram"
+                alt="Instagram"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/washington-luiz-de-souza-1a930868/"
+                title="Linkedin"
+                alt="Linkedin"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
